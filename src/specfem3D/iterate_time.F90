84299f8af86e91e2539a820c932251934aa3943b
homnath <hngharti@gmail.com>
2021-06-11 11:25:59 -0400

    - revert to interval check stability for efficiency

--- src/specfem3D/iterate_time.F90
+++ src/specfem3D/iterate_time.F90
@@ -180,9 +180,9 @@
   do it = it_begin,it_end
 
     ! simulation status output and stability check
-    !if (mod(it,NTSTEP_BETWEEN_OUTPUT_INFO) == 0 .or. it == it_begin + 4 .or. it == it_end) then
+    if (mod(it,NTSTEP_BETWEEN_OUTPUT_INFO) == 0 .or. it == it_begin + 4 .or. it == it_end) then
       call check_stability()
-    !endif
+    endif
 
     ! simulation status output and stability check
     if (OUTPUT_ENERGY) then
